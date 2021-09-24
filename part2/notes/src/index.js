faf457913ff55fdf9aefe3bc3926d2be3d0ceef0
hanchen.ye <hanchen.ye@pi-solution.com>
2021-09-24 09:37:59 +0800

    add

--- part2/notes/src/index.js
+++ part2/notes/src/index.js
@@ -2,13 +2,6 @@ import ReactDOM from 'react-dom'
 import App from './App';
 import './index.css';
 
-// const express = require('express');
-// const app = express();
-// app.use(express.json());
-
-// const cors = require('cors');
-// app.use(cors());
-
 ReactDOM.render(
     <App />,
     document.getElementById('root')
