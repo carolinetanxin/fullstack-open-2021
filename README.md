a99e96fb3e8e5d638c73a445c647c576fcdbd436
hanchen.ye <hanchen.ye@pi-solution.com>
2021-10-09 15:01:29 +0800

    part4/b:4.8-4.14

--- README.md
+++ README.md
@@ -12,3 +12,14 @@ npm start
 2. git push code by running
 `git push --set-upstream origin main`
 
+
+3. npm test指令
+某文件 `npm test -- tests/note_api.test.js`
+某特定名称的测试 `npm test -- -t "a specific note is within the returned notes"`
+
+4. npm test 遭遇到的报错信息
+```
+thrown: "Exceeded timeout of 5000 ms for a hook.
+Use jest.setTimeout(newTimeout) to increase the timeout value, if this is a long-running test."
+```     
+解决方案： 在beforeEach方法上面添加`jest.setTimeout(100000)`
--- part4/blog-list/controllers/blog.js
+++ part4/blog-list/controllers/blog.js
@@ -2,50 +2,44 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', (request, response) => {
-    Blog.find({}).then(blogs => {
-        response.json(blogs)
-    })
+blogRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
-blogRouter.get('/:id', (request, response, next) => {
-    Blog.findById(request.params.id)
-        .then(blog => {
-            if (blog) {
-                response.json(blog)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
-})
-
-blogRouter.post('/', (request, response, next) => {
+blogRouter.post('/', async (request, response, next) => {
     const body = request.body
 
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes
-    })
-
-    blog.save()
-        .then(savedBlog => {
-            response.json(savedBlog)
+    if (!body.title || !body.url) {
+        response.status(400).end()
+    } else {
+        const blog = new Blog({
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: body.likes || 0
         })
-        .catch(error => next(error))
+
+        const savedBlog = await blog.save()
+        response.json(savedBlog)
+    }
 })
 
-blogRouter.delete('/:id', (request, response, next) => {
-    Blog.findByIdAndRemove(request.params.id)
-        .then(() => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+blogRouter.get('/:id', async (request, response, next) => {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+        response.json(blog)
+    } else {
+        response.status(404).end()
+    }
 })
 
-blogRouter.put('/:id', (request, response, next) => {
+blogRouter.delete('/:id', async (request, response, next) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+})
+
+blogRouter.put('/:id', async (request, response, next) => {
     const body = request.body
 
     const blog = {
@@ -55,11 +49,8 @@ blogRouter.put('/:id', (request, response, next) => {
         likes: body.likes
     }
 
-    Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-        .then(updatedBlog => {
-            response.json(updatedBlog)
-        })
-        .catch(error => next(error))
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+    response.json(updatedBlog)
 })
 
 module.exports = blogRouter
